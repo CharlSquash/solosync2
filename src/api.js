c92a6ac5a8e2bf1d312297c76f105934dbefff23
Charl <charl.squash@gmail.com>
2026-01-16 07:18:02 +0200

    persistent login changes 90 days

--- src/api.js
+++ src/api.js
@@ -2,6 +2,23 @@
 import axios from 'axios';
 import authApi from './authApi';
 
+// Flag to prevent multiple simultaneous refreshes
+let isRefreshing = false;
+// Queue to hold requests that are waiting for the token refresh
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+
+  failedQueue = [];
+};
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
@@ -25,25 +42,50 @@ api.interceptors.response.use(
   },
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+
+    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+      if (isRefreshing) {
+        return new Promise(function(resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers['Authorization'] = 'Bearer ' + token;
+            return api(originalRequest);
+          })
+          .catch((err) => {
+            return Promise.reject(err);
+          });
+      }
+
       originalRequest._retry = true;
+      isRefreshing = true;
+
       try {
         const refreshToken = localStorage.getItem('refresh_token');
         const response = await authApi.post('/api/token/refresh/', {
           refresh: refreshToken,
         });
+
         const newAccessToken = response.data.access;
         localStorage.setItem('access_token', newAccessToken);
         api.defaults.headers.common['Authorization'] = 'Bearer ' + newAccessToken;
+        
+        processQueue(null, newAccessToken);
+        
         return api(originalRequest);
       } catch (refreshError) {
+        processQueue(refreshError, null);
+        
         // If refresh fails, logout the user
         localStorage.removeItem('access_token');
         localStorage.removeItem('refresh_token');
         window.location.href = '/login';
         return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
       }
     }
+
     return Promise.reject(error);
   }
 );
